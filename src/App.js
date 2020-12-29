c2b049adcca1bdf93f0ca81d35ff098b61c394fd
Valeria Ramey <valeriaramey@gmail.com>
2020-12-28 20:19:19 -0500

    adds style to cards and header components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import './index.js';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div id="body">
+        <Header/>
+        <Card
+           className='section'
+           img="./about.png"
+           title='About Us'
+           description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Aliquet porttitor lacus luctus accumsan
+                        tortor posuere. Sodales ut eu sem integer. Commodo quis imperdiet massa tincidunt nunc.
+                        Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt.'
+        />
+
+        <Card
+           className='section'
+           img="./values.png"
+           title='Our Values'
+           description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Aliquet porttitor lacus luctus accumsan
+                        tortor posuere. Sodales ut eu sem integer. Commodo quis imperdiet massa tincidunt nunc.
+                        Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt.'
+        />
+        <Card
+           className='section'
+           img="./mission.png"
+           title='Our Mission'
+           description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Aliquet porttitor lacus luctus accumsan
+                        tortor posuere. Sodales ut eu sem integer. Commodo quis imperdiet massa tincidunt nunc.
+                        Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt.'
+        />
+
+        <ContactContainer/>
+     </div>
+     );
+   }
+
+
+const Header = () => {
+  return(
+     <div className='header'>
+       <span className='header-title'>
+          COMPANY
+       </span>
+       <br/>
+       <span className='header-text'>
+         Lorem ipsum dolor sit amet
+       </span>
+     </div>
   );
 }
 
-export default App;
+
+
+const Card = (props) => {
+  return(
+    <div className={props.className}>
+      <div className='small-div'>
+        <i className={props.className}></i>
+        <img src={props.img} alt=''className="card-img"/>
+      </div>
+
+      <div className="big-div">
+        <span className='div-title'>
+          {props.title}
+         </span>
+         <br/>
+         <span>
+           {props.description}
+         </span>
+      </div>
+    </div>
+    )
+}
+
+
+const ContactContainer = () => {
+  return (
+    <div className="contact-div">
+      <span className='contact-title'>
+        Contact us
+      </span>
+      <div className="contact-info">
+        <h2>Contact us and we will get back to you soon</h2>
+        <h2>Brooklyn, NY</h2>
+        <h2>123-456-7890</h2>
+        <h2>company@gmail.com</h2>
+      </div>
+      <div className='contact-input'>
+        <span>
+            Contact
+        </span>
+
+        <input type="text" placeholder="email address" className="input-field"/>
+        <textarea name="" id="" cols="30" rows="10" placeholder="comment"></textarea>
+        <button className="contact-btn">Send</button>
+      </div>
+    </div>
+
+    )
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,57 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App.js";
 
-// Body component that holds other components
 
-const Body= ()=> {
-  return(
-     <div id="body">
-        <Header/>
-        <Card
-           className='section'
-           img=""
-        />
-        <ContactContainer/>
-     </div>
-     )
-}
 
-// Hearder component
 
-const Header = () => {
-  return(
-     <div className='header'>
-       <span className='header-title'>
-          Company
-       </span>
-       <br/>
-       <span className='header-text'>
-         We specialize in....
-       </span>
-     </div>
-  );
-}
 
-// Card component
 
-const Card = (props) => {
-  return(
-    <div className={props.className}>
-      <div className='small-div'>
-        <i className={props.className}></i>
-        <img src={props.img} alt=''/>
-      </div>
-
-      <div className="big-div">
-        <span className='div-tittle'>
-          {prop.title}
-         </span>
-         <br/>
-         <span>
-           {props.description}
-         </span>
-      </div>
-    </div>
-    )
-}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
